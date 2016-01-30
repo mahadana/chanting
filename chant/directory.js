c3bc3ca6b7118fe5344c6909747fd434c0d2bdb4
Jeff Zira <jeff@lark.com>
2016-01-29 23:00:33 -0800

    Add more chants

--- chant/directory.js
+++ chant/directory.js
@@ -1,6 +1,6 @@
 // load up object of types.
 // Allows access via "types.english"
-var typeArray = ["pali","english","chinese","metta","mantra"];
+var typeArray = ["pali","english","chinese","metta","mantra","sanskrit","translated"];
 var t = {};
 for(var i=0;i<typeArray.length;i++) {
   t[typeArray[i]] = i;
@@ -28,4 +28,25 @@ var Directory = [
     types:[t.chinese,t.mantra],
     title:"Na Mo Kuan Shir Yin (x8)"
   },
+  {
+    id:5,
+    types:[t.pali,t.mantra],
+    title:"Namo Tassa Bhagavato (x3)"
+  },
+  {
+    id:6,
+    types:[t.pali],
+    title:"Refuges (Buddham saranam gacchami)"
+  },
+  {
+    id:7,
+    types:[t.pali,t.translated],
+    title:"The 5 Precepts (with translation)"
+  },
+  {
+    id:8,
+    types:[t.sanskrit,t.mantra],
+    title:"Gate Gate Paragate"
+  },
+  
 ];
